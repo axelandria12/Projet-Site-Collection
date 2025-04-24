418d94e262eca4927400886a86d425e44b6badf0
Tim CREBESSEGUES <tim.crebessegues@edouardgand.fr>
2025-04-24 13:59:40 +0200

    Ajout de la fonction getBase

---
+++ Backend/data.php
@@ -0,0 +1,14 @@
+<?php
+    function getBase($login, $mdp, $bd, $server) {
+        try {
+            $result = new PDO("mysql:host=$server;dbname=$bd", $login, $mdp, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'')); 
+            $result->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $result;
+        } catch (PDOException $e) {
+            print "Erreur de connexion PDO ";
+            die();
+        }
+    }
+    
+
+?>
