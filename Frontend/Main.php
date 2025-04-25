ea1862183dc99cab6c0586d1146c1e799373f22f
Tim CREBESSEGUES <tim.crebessegues@edouardgand.fr>
2025-04-25 08:37:27 +0200

    Changement de certain nom

---
+++ Frontend/Main.php
@@ -0,0 +1,5 @@
+<?php
+include "$racine/front end/vue/enteteCollec.html.php";
+include "$racine/front end/vue/vueAuthentification.php";
+include "$racine/front end/vue/pied.html.php";
+?>
--- front end/Menu.php
+++
@@ -1,5 +0,0 @@
-<?php
-include "$racine/vue/enteteCollec.html.php";
-include "$racine/vue/vueAuthentification.php";
-include "$racine/vue/pied.html.php";
-?>
